51d9cec5f7b5bef405e6a133a0b880d2a5b2ec60
Pascal Sommer <p@pascalsommer.ch>
2016-04-19 18:28:52 +0200

    blog index seperated in to functions

--- blog/index.php
+++ blog/index.php
@@ -1,3 +1,65 @@
+<?php
+
+  $mysql_username = "root";//"pascalsommer_ch";
+  $mysql_password = "";//"Nosvctxk";
+  $mysql_database = "pascalsommer_ch";
+
+
+  $entries = scandir("entries", 1);
+  $month = array("", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+  function printPost($entry, $conn){
+    global $mysql_username, $mysql_password, $mysql_database, $month;
+    $nameparts = explode("_", $entry);
+    $title = str_replace("-", " ", $nameparts[1]);
+    $dateparts = explode(".",$nameparts[0]);
+    $datestring = $dateparts[2] . ". " . $month[intval($dateparts[1])] . " " . $dateparts[0];
+
+    print("<div class='entry'>");
+    print("  <h2>".$title."</h2><hr>");
+    print("  <div class='entrycontent'>");
+
+    // -------------------- Pictures ----------------
+
+    $pics=scandir("entries/".$entry);
+    foreach($pics as $p){
+      if($p!="." && $p!=".."){
+        print("<img src = 'entries/".$entry."/".$p."'>");
+      }
+    }
+    print("  </div><hr>");
+    print("  <div class='lower'><p class='date'>".$datestring."</p>");
+
+
+    // -------------------- Comments -----------------
+
+    $query="select name, content, date_format(time, '%e.%c.%Y') as date from comments where post = '". $entry . "'";
+    $result = $conn->query($query);
+    if($result->num_rows>0){
+      if($result->num_rows>1){
+        print("<p class='togglecomments'>".$result->num_rows." comments</p>");
+      }else{
+        print("<p class='togglecomments'>1 comment</p>");
+      }
+      print("<div class='comments'>");
+      while($row=$result->fetch_assoc()){
+        print("<div class='comment'><div class='date'>".$row["date"]."</div>");
+        print("<h3>".htmlspecialchars($row["name"])."</h3><p>".nl2br(htmlspecialchars($row["content"]))."</p></div>");
+      }
+      print("</div>");
+    }else{
+      print("<p class='togglecomments'>no comments yet</p>");
+      print("<div class='comments'></div>");
+    }
+    print("<div class='newcomment'><form method='POST' action='postcomment.php'>");
+    print("<input type='hidden' name='post' value='" . $entry . "'>");
+    print("<p>Name:</p><input type='text' placeholder='' name='name'><br>");
+    print("<p>Comment:</p><textarea name='content'></textarea><br>");
+    print("<input type='submit' value='submit'></form>");
+    print("</div></div></div>");
+  }
+?>
+
 <html>
 <head>
   <meta charset="utf-8">
@@ -15,7 +77,7 @@
 <body>
   <div style="display:none;" id="phpinfo">
     <?php
-      print(count(scandir("entries"))-2);
+      print(count($entries)-2);
     ?></div>
 <div style="display:none;" id="args"><?php
 if(isset($_GET["show"])){
@@ -44,64 +106,18 @@ if(isset($_GET["start"])){
 
 <div id="newer" class="button">show newer posts</div>
 
-<div id="blog"><?php
-     $entries = scandir("entries", 1);
-     $month = array("", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-     $conn = new mysqli("localhost", "pascalsommer_ch", "Nosvctxk", "pascalsommer_ch");
+<div id="blog">
+<?php     
+  $conn = new mysqli("localhost", $mysql_username, $mysql_password, $mysql_database);
 
-     foreach($entries as $e){
-       
-       if($e!="." && $e!=".."){
-         $nameparts = explode("_", $e);
-	 $title = str_replace("-", " ", $nameparts[1]);
-      	 $dateparts = explode(".",$nameparts[0]);
-      	 $datestring = $dateparts[2] . ". " . $month[intval($dateparts[1])] . " " . $dateparts[0];
-      	 print("<div class='entry'>");
-	 print("  <h2>".$title."</h2><hr>");
-	 print("  <div class='entrycontent'>");
-	 
-	 $pics=scandir("entries/".$e);
-	 foreach($pics as $p){
-	   if($p!="." && $p!=".."){
-             print("<img src = 'entries/".$e."/".$p."'>");
-           }
-      	 }
-	 print("  </div><hr>");
-	 print("  <div class='lower'><p class='date'>".$datestring."</p>");
-         
-	 $query="select name, content, date_format(time, '%e.%c.%Y') as date from comments where post = '". $e . "'";
-    	 $result = $conn->query($query);
-  
-	 if($result->num_rows>0){
-	   if($result->num_rows>1){
-             print("<p class='togglecomments'>".$result->num_rows." comments</p>");
-	   }else{
-             print("<p class='togglecomments'>1 comment</p>");
-           }
-           print("<div class='comments'>");
-	   while($row=$result->fetch_assoc()){
-	     print("<div class='comment'><div class='date'>".$row["date"]."</div>");
-	     print("<h3>".htmlspecialchars($row["name"])."</h3><p>".nl2br(htmlspecialchars($row["content"]))."</p></div>");
-	   }
-	   print("</div>");
-	 }else{
-	   print("<p class='togglecomments'>no comments yet</p>");
-	   print("<div class='comments'></div>");
-	   
-	 }
-  
-	 print("<div class='newcomment'><form method='POST' action='postcomment.php'>");
-	 print("<input type='hidden' name='post' value='" . $e . "'>");
-	 print("<p>Name:</p><input type='text' placeholder='' name='name'><br>");
-	 print("<p>Comment:</p><textarea name='content'></textarea><br>");
-	 print("<input type='submit' value='submit'></form>");
-	 print("</div></div></div>");
-       }
-       
-     }
-     $conn->close();
-     
-     ?>
+  foreach($entries as $e){   
+    if($e!="." && $e!=".."){        
+      printPost($e, $conn);
+    }      
+  }
+  $conn->close();
+
+?>
 
 <div id="older" class="button">show older posts</div>
 </div>
