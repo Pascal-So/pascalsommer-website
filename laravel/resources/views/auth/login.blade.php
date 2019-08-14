38e7a89d37a92bf88bfc28fffec4b5dbb496d3dd
Pascal Sommer <p@pascalsommer.ch>
2019-08-14 18:11:02 +0200

    login view: readd 'remember me' checkbox

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -11,9 +11,16 @@
     @include('layouts.errors')
     <br>
 
+    <style type="text/css">
+        .label-column {
+            text-align: right;
+            padding-right: 8px;
+        }
+    </style>
+
     <table class="inline-block">
         <tr>
-            <td style="text-align: right">
+            <td class="label-column">
                 <label for="name">Name </label>
             </td>
             <td>
@@ -21,11 +28,19 @@
             </td>
         </tr>
         <tr>
-            <td style="text-align: right">
+            <td class="label-column">
                 <label for="password">Password </label>
             </td>
             <td>
                 <input id="password" type="password" name="password" value="{{ old('password') }}" required>
+            </td>
+        </tr>
+        <tr>
+            <td class="label-column">
+                <label for="remember">Remember Me </label>
+            </td>
+            <td style="text-align: left">
+                <input id="remember" type="checkbox" name="remember" {{ old('remember', true) ? 'checked' : '' }}>
             </td>
         </tr>
 
