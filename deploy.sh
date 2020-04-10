67529695e3afdc84940eb25f1fba97cc4c5f83af
Pascal Sommer <p@pascalsommer.ch>
2020-04-10 20:28:41 +0200

    change vendor dir to be uploaded via rsync instead of git-ftp

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
-(cd laravel; yarn install --frozen-lockfile; npm run prod)
+cd laravel
+
+yarn install --frozen-lockfile
+npm run prod
+
+composer install
+rsync -a -vvi -zz --progress --delete vendor/ pascal:/var/www/photography/laravel/vendor
+
+cd ..
 
 git ftp push
